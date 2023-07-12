2dd667556e9133aaf82bf3c421b5d6d9e5ba90a0
Kevin Black <kevin3.black@gmail.com>
2023-07-12 11:44:50 -0700

    Update bridge conversion script
    - Add language annotation
    - Add all camera views, with empty strings for missing views
    - More shuffling

--- scripts/bridgedata/raw_to_tfrecord.py
+++ scripts/bridgedata/raw_to_tfrecord.py
@@ -43,7 +43,8 @@ import glob
 import os
 import pickle
 import numpy as np
-from absl import app, flags, logging
+from absl import app, flags
+import logging
 import tqdm
 import random
 from multiprocessing import Pool
@@ -62,14 +63,12 @@ flags.DEFINE_integer(
     "{input_path}/dir_1/dir_2/.../dir_{depth-1}/2022-01-01_00-00-00/...",
 )
 flags.DEFINE_bool("overwrite", False, "Overwrite existing files")
-flags.DEFINE_float(
-    "train_proportion", 0.9, "Proportion of data to use for training (rather than val)"
-)
+flags.DEFINE_float("train_proportion", 0.9, "Proportion of data to use for training (rather than val)")
 flags.DEFINE_integer("num_workers", 8, "Number of threads to use")
 flags.DEFINE_integer("shard_size", 200, "Maximum number of trajectories per shard")
 
 IMAGE_SIZE = (128, 128)
-CAMERA_VIEWS = {"images0"}
+CAMERA_VIEWS = {"images0", "images1", "images2"}
 
 
 def process_images(path):  # processes images at a trajectory level
@@ -85,10 +84,15 @@ def process_images(path):  # processes images at a trajectory level
     filenames = [[path.split("/")[-1] for path in x] for x in image_paths]
     assert all(x == filenames[0] for x in filenames)
 
-    return {
+    d = {
         image_dir: [read_resize_encode_image(path, IMAGE_SIZE) for path in p]
         for image_dir, p in zip(image_dirs, image_paths)
     }
+
+    for missing in CAMERA_VIEWS - set(d.keys()):
+        d[missing] = [""] * len(image_paths[0])  # empty string is a placeholder for missing images
+
+    return d
 
 
 def process_state(path):
@@ -107,6 +111,16 @@ def process_actions(path):
     return act_list
 
 
+def process_lang(path):
+    fp = os.path.join(path, "lang.txt")
+    text = ""  # empty string is a placeholder for missing text
+    if os.path.exists(fp):
+        with open(fp, "r") as f:
+            text = f.readline().strip()
+
+    return text
+
+
 # create a tfrecord for a group of trajectories
 def create_tfrecord(paths, output_path, tqdm_func, global_tqdm):
     writer = tf.io.TFRecordWriter(output_path)
@@ -121,6 +135,7 @@ def create_tfrecord(paths, output_path, tqdm_func, global_tqdm):
             out["obs"] = process_images(path)
             out["obs"]["state"] = process_state(path)
             out["actions"] = process_actions(path)
+            out["lang"] = process_lang(path)
 
             # shift the actions according to camera latency
             if latency_shift:
@@ -130,18 +145,11 @@ def create_tfrecord(paths, output_path, tqdm_func, global_tqdm):
             # append a null action to the end
             out["actions"].append(np.zeros_like(out["actions"][0]))
 
-            assert (
-                len(out["actions"])
-                == len(out["obs"]["state"])
-                == len(out["obs"]["images0"])
-            )
+            assert len(out["actions"]) == len(out["obs"]["state"]) == len(out["obs"]["images0"])
 
             example = tf.train.Example(
                 features=tf.train.Features(
-                    feature={
-                        k: tensor_feature(v)
-                        for k, v in dl.transforms.flatten_dict(out).items()
-                    }
+                    feature={k: tensor_feature(v) for k, v in dl.transforms.flatten_dict(out).items()}
                 )
             )
             writer.write(example.SerializeToString())
@@ -194,40 +202,26 @@ def main(_):
 
     # get trajecotry paths in parallel
     with Pool(FLAGS.num_workers) as p:
-        train_paths, val_paths = zip(
-            *p.map(
-                partial(get_traj_paths, train_proportion=FLAGS.train_proportion), paths
-            )
-        )
+        train_paths, val_paths = zip(*p.map(partial(get_traj_paths, train_proportion=FLAGS.train_proportion), paths))
 
     train_paths = [x for y in train_paths for x in y]
     val_paths = [x for y in val_paths for x in y]
+    random.shuffle(train_paths)
+    random.shuffle(val_paths)
 
     # shard paths
-    train_shards = np.array_split(
-        train_paths, np.ceil(len(train_paths) / FLAGS.shard_size)
-    )
+    train_shards = np.array_split(train_paths, np.ceil(len(train_paths) / FLAGS.shard_size))
     val_shards = np.array_split(val_paths, np.ceil(len(val_paths) / FLAGS.shard_size))
 
     # create output paths
     tf.io.gfile.makedirs(os.path.join(FLAGS.output_path, "train"))
     tf.io.gfile.makedirs(os.path.join(FLAGS.output_path, "val"))
-    train_output_paths = [
-        os.path.join(FLAGS.output_path, "train", f"{i}.tfrecord")
-        for i in range(len(train_shards))
-    ]
-    val_output_paths = [
-        os.path.join(FLAGS.output_path, "val", f"{i}.tfrecord")
-        for i in range(len(val_shards))
-    ]
+    train_output_paths = [os.path.join(FLAGS.output_path, "train", f"{i}.tfrecord") for i in range(len(train_shards))]
+    val_output_paths = [os.path.join(FLAGS.output_path, "val", f"{i}.tfrecord") for i in range(len(val_shards))]
 
     # create tasks (see tqdm_multiprocess documenation)
-    tasks = [
-        (create_tfrecord, (train_shards[i], train_output_paths[i]))
-        for i in range(len(train_shards))
-    ] + [
-        (create_tfrecord, (val_shards[i], val_output_paths[i]))
-        for i in range(len(val_shards))
+    tasks = [(create_tfrecord, (train_shards[i], train_output_paths[i])) for i in range(len(train_shards))] + [
+        (create_tfrecord, (val_shards[i], val_output_paths[i])) for i in range(len(val_shards))
     ]
 
     # run tasks
