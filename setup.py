904c90061663e40b9e2ab15e423f12773721b35c
Kevin Black <kevin3.black@gmail.com>
2023-08-28 18:14:59 -0700

    Fix setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="dlimp",
     version="0.0.1",
-    packages=["dlimp"],
+    packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
         "tensorflow>=2.13.0",
         "tqdm",
         "tqdm-multiprocess==0.0.11",
         "pre-commit==3.3.3",
+        "tensorflow_datasets>=4.9.2",
     ],
 )
